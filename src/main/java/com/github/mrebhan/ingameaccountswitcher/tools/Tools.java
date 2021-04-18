1bb9caea31654d5a1bc387b5bb6c6728ca1a552c
VidTuGit <60929459+VidTuGit@users.noreply.github.com>
2021-04-18 20:22:41 +0300

    Forge 1.14 release

--- src/main/java/com/github/mrebhan/ingameaccountswitcher/tools/Tools.java
+++ src/main/java/com/github/mrebhan/ingameaccountswitcher/tools/Tools.java
@@ -1,6 +1,5 @@
 package com.github.mrebhan.ingameaccountswitcher.tools;
-
-import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.screen.Screen;
 
 /**
  * @author MRebhan
@@ -26,10 +25,10 @@ public class Tools {
 	 * inside color(ARGB format)
 	 */
 	public static void drawBorderedRect(int x, int y, int x1, int y1, int size, int borderColor, int insideColor) {
-		Gui.drawRect(x + size, y + size, x1 - size, y1 - size, insideColor);
-		Gui.drawRect(x + size, y + size, x1, y, borderColor);
-		Gui.drawRect(x, y, x + size, y1, borderColor);
-		Gui.drawRect(x1, y1, x1 - size, y + size, borderColor);
-		Gui.drawRect(x, y1 - size, x1, y1, borderColor);
+		Screen.fill(x + size, y + size, x1 - size, y1 - size, insideColor);
+		Screen.fill(x + size, y + size, x1, y, borderColor);
+		Screen.fill(x, y, x + size, y1, borderColor);
+		Screen.fill(x1, y1, x1 - size, y + size, borderColor);
+		Screen.fill(x, y1 - size, x1, y1, borderColor);
 	}
 }
--- src/main/java/the_fireplace/ias/IAS.java
+++ src/main/java/the_fireplace/ias/IAS.java
@@ -1,56 +1,54 @@
 package the_fireplace.ias;
 
 import com.github.mrebhan.ingameaccountswitcher.MR;
-import net.minecraft.client.resources.I18n;
+
+import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.common.config.Property;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import ru.vidtu.iasfork.IASConfigScreen;
 import the_fireplace.ias.config.ConfigValues;
 import the_fireplace.ias.events.ClientEvents;
-import the_fireplace.ias.tools.Reference;
 import the_fireplace.ias.tools.SkinTools;
 import the_fireplace.iasencrypt.Standards;
 /**
  * @author The_Fireplace
  */
-@Mod(modid=Reference.MODID, name=Reference.MODNAME, clientSideOnly=true, guiFactory="the_fireplace.ias.config.IASGuiFactory", updateJSON = "http://thefireplace.bitnamiapp.com/jsons/ias.json", acceptedMinecraftVersions = "[1.11,)")
+@Mod("ias")
 public class IAS {
-	public static Configuration config;
-	private static Property CASESENSITIVE_PROPERTY;
-	private static Property ENABLERELOG_PROPERTY;
-
-	public static void syncConfig(){
-		ConfigValues.CASESENSITIVE = CASESENSITIVE_PROPERTY.getBoolean();
-		ConfigValues.ENABLERELOG = ENABLERELOG_PROPERTY.getBoolean();
-		if(config.hasChanged())
-			config.save();
+	public static ForgeConfigSpec config;
+	public static ForgeConfigSpec.BooleanValue CASESENSITIVE_PROPERTY;
+	public static ForgeConfigSpec.BooleanValue ENABLERELOG_PROPERTY;
+	public static void syncConfig(boolean save) {
+		ConfigValues.CASESENSITIVE = CASESENSITIVE_PROPERTY.get();
+		ConfigValues.ENABLERELOG = ENABLERELOG_PROPERTY.get();
+		if (save) config.save();
 	}
-
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		config = new Configuration(event.getSuggestedConfigurationFile());
-		config.load();
-		CASESENSITIVE_PROPERTY = config.get(Configuration.CATEGORY_GENERAL, ConfigValues.CASESENSITIVE_NAME, ConfigValues.CASESENSITIVE_DEFAULT, I18n.format(ConfigValues.CASESENSITIVE_NAME+".tooltip"));
-		ENABLERELOG_PROPERTY = config.get(Configuration.CATEGORY_GENERAL, ConfigValues.ENABLERELOG_NAME, ConfigValues.ENABLERELOG_DEFAULT, I18n.format(ConfigValues.ENABLERELOG_NAME+".tooltip"));
-		syncConfig();
-		if(!event.getModMetadata().version.equals("${version}"))//Dev environment needs to use a local list, to avoid issues
+	
+	public IAS() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (mc, s) -> new IASConfigScreen(s));
+		ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+		CASESENSITIVE_PROPERTY = builder.comment("Should account searches be case sensitive?").define(ConfigValues.CASESENSITIVE_NAME, ConfigValues.CASESENSITIVE_DEFAULT);
+		ENABLERELOG_PROPERTY = builder.comment("Enables logging in to the account you are already logged in to.").define(ConfigValues.ENABLERELOG_NAME, ConfigValues.ENABLERELOG_DEFAULT);
+		config = builder.build();
+		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, config);
+		syncConfig(false);
+		try {
+			Class.forName("net.minecraft.util.math.MathHelper");
+		} catch (Throwable t) {
 			Standards.updateFolder();
-		else
-			System.out.println("Dev environment detected!");
-	}
-	@EventHandler
-	public void init(FMLInitializationEvent event){
+		}
 		MR.init();
 		MinecraftForge.EVENT_BUS.register(new ClientEvents());
 		Standards.importAccounts();
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientComplete);
 	}
-	@EventHandler
-	public void postInit(FMLPostInitializationEvent event){
-		SkinTools.cacheSkins();
+	
+	public void onClientComplete(FMLLoadCompleteEvent event) {
+		SkinTools.cacheSkins(false);
 	}
 }
