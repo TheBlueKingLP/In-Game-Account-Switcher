c6a2cedd970d41577898863ada815e299ad9b2ab
VidTuGit <60929459+VidTuGit@users.noreply.github.com>
2021-04-25 18:19:17 +0300

    Update to 7.0.6

--- src/main/java/the_fireplace/ias/tools/SkinTools.java
+++ src/main/java/the_fireplace/ias/tools/SkinTools.java
@@ -2,7 +2,6 @@ package the_fireplace.ias.tools;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -13,6 +12,8 @@ import java.util.Base64;
 
 import javax.imageio.ImageIO;
 
+import org.lwjgl.glfw.GLFW;
+
 import com.github.mrebhan.ingameaccountswitcher.tools.alt.AccountData;
 import com.github.mrebhan.ingameaccountswitcher.tools.alt.AltDatabase;
 import com.google.gson.Gson;
@@ -20,9 +21,10 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SharedConstants;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fml.StartupMessageManager;
 
 /**
  * Tools that have to do with Skins
@@ -34,74 +36,70 @@ public class SkinTools {
 	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 	public static final File cachedir = new File(Minecraft.getInstance().gameDir, "cachedImages/skins/");
 	private static final File skinOut = new File(cachedir, "temp.png");
-	
 	public static void buildSkin(String name) {
-		BufferedImage skin;
 		try {
-			skin = ImageIO.read(new File(cachedir, name + ".png"));
-		} catch (IOException e) {
-			if (skinOut.exists())
-				skinOut.delete();
-			return;
-		}
-		BufferedImage drawing = new BufferedImage(16, 32, BufferedImage.TYPE_INT_ARGB);
-		if (skin.getHeight() == 64) {// New skin type
-			int[] head = skin.getRGB(8, 8, 8, 8, null, 0, 8);
-			int[] torso = skin.getRGB(20, 20, 8, 12, null, 0, 8);
-			int[] larm = skin.getRGB(44, 20, 4, 12, null, 0, 4);
-			int[] rarm = skin.getRGB(36, 52, 4, 12, null, 0, 4);
-			int[] lleg = skin.getRGB(4, 20, 4, 12, null, 0, 4);
-			int[] rleg = skin.getRGB(20, 52, 4, 12, null, 0, 4);
-			int[] hat = skin.getRGB(40, 8, 8, 8, null, 0, 8);
-			int[] jacket = skin.getRGB(20, 36, 8, 12, null, 0, 8);
-			int[] larm2 = skin.getRGB(44, 36, 4, 12, null, 0, 4);
-			int[] rarm2 = skin.getRGB(52, 52, 4, 12, null, 0, 4);
-			int[] lleg2 = skin.getRGB(4, 36, 4, 12, null, 0, 4);
-			int[] rleg2 = skin.getRGB(4, 52, 4, 12, null, 0, 4);
-			for (int i = 0; i < hat.length; i++)
-				if (hat[i] == 0)
-					hat[i] = head[i];
-			for (int i = 0; i < jacket.length; i++)
-				if (jacket[i] == 0)
-					jacket[i] = torso[i];
-			for (int i = 0; i < larm2.length; i++)
-				if (larm2[i] == 0)
-					larm2[i] = larm[i];
-			for (int i = 0; i < rarm2.length; i++)
-				if (rarm2[i] == 0)
-					rarm2[i] = rarm[i];
-			for (int i = 0; i < lleg2.length; i++)
-				if (lleg2[i] == 0)
-					lleg2[i] = lleg[i];
-			for (int i = 0; i < rleg2.length; i++)
-				if (rleg2[i] == 0)
-					rleg2[i] = rleg[i];
-			drawing.setRGB(4, 0, 8, 8, hat, 0, 8);
-			drawing.setRGB(4, 8, 8, 12, jacket, 0, 8);
-			drawing.setRGB(0, 8, 4, 12, larm2, 0, 4);
-			drawing.setRGB(12, 8, 4, 12, rarm2, 0, 4);
-			drawing.setRGB(4, 20, 4, 12, lleg2, 0, 4);
-			drawing.setRGB(8, 20, 4, 12, rleg2, 0, 4);
-		} else {// old skin type
-			int[] head = skin.getRGB(8, 8, 8, 8, null, 0, 8);
-			int[] torso = skin.getRGB(20, 20, 8, 12, null, 0, 8);
-			int[] arm = skin.getRGB(44, 20, 4, 12, null, 0, 4);
-			int[] leg = skin.getRGB(4, 20, 4, 12, null, 0, 4);
-			int[] hat = skin.getRGB(40, 8, 8, 8, null, 0, 8);
-			for (int i = 0; i < hat.length; i++)
-				if (hat[i] == 0)
-					hat[i] = head[i];
-			drawing.setRGB(4, 0, 8, 8, hat, 0, 8);
-			drawing.setRGB(4, 8, 8, 12, torso, 0, 8);
-			drawing.setRGB(0, 8, 4, 12, arm, 0, 4);
-			drawing.setRGB(12, 8, 4, 12, arm, 0, 4);
-			drawing.setRGB(4, 20, 4, 12, leg, 0, 4);
-			drawing.setRGB(8, 20, 4, 12, leg, 0, 4);
-		}
-		try {
+			File f = new File(cachedir, name + ".png");
+			if (!f.exists()) loadFromMojang(Minecraft.getInstance(), name, f);
+			BufferedImage skin = ImageIO.read(f);
+			BufferedImage drawing = new BufferedImage(16, 32, BufferedImage.TYPE_INT_ARGB);
+			if (skin.getHeight() == 64) {// New skin type
+				int[] head = skin.getRGB(8, 8, 8, 8, null, 0, 8);
+				int[] torso = skin.getRGB(20, 20, 8, 12, null, 0, 8);
+				int[] larm = skin.getRGB(44, 20, 4, 12, null, 0, 4);
+				int[] rarm = skin.getRGB(36, 52, 4, 12, null, 0, 4);
+				int[] lleg = skin.getRGB(4, 20, 4, 12, null, 0, 4);
+				int[] rleg = skin.getRGB(20, 52, 4, 12, null, 0, 4);
+				int[] hat = skin.getRGB(40, 8, 8, 8, null, 0, 8);
+				int[] jacket = skin.getRGB(20, 36, 8, 12, null, 0, 8);
+				int[] larm2 = skin.getRGB(44, 36, 4, 12, null, 0, 4);
+				int[] rarm2 = skin.getRGB(52, 52, 4, 12, null, 0, 4);
+				int[] lleg2 = skin.getRGB(4, 36, 4, 12, null, 0, 4);
+				int[] rleg2 = skin.getRGB(4, 52, 4, 12, null, 0, 4);
+				for (int i = 0; i < hat.length; i++)
+					if (hat[i] == 0)
+						hat[i] = head[i];
+				for (int i = 0; i < jacket.length; i++)
+					if (jacket[i] == 0)
+						jacket[i] = torso[i];
+				for (int i = 0; i < larm2.length; i++)
+					if (larm2[i] == 0)
+						larm2[i] = larm[i];
+				for (int i = 0; i < rarm2.length; i++)
+					if (rarm2[i] == 0)
+						rarm2[i] = rarm[i];
+				for (int i = 0; i < lleg2.length; i++)
+					if (lleg2[i] == 0)
+						lleg2[i] = lleg[i];
+				for (int i = 0; i < rleg2.length; i++)
+					if (rleg2[i] == 0)
+						rleg2[i] = rleg[i];
+				drawing.setRGB(4, 0, 8, 8, hat, 0, 8);
+				drawing.setRGB(4, 8, 8, 12, jacket, 0, 8);
+				drawing.setRGB(0, 8, 4, 12, larm2, 0, 4);
+				drawing.setRGB(12, 8, 4, 12, rarm2, 0, 4);
+				drawing.setRGB(4, 20, 4, 12, lleg2, 0, 4);
+				drawing.setRGB(8, 20, 4, 12, rleg2, 0, 4);
+			} else {// old skin type
+				int[] head = skin.getRGB(8, 8, 8, 8, null, 0, 8);
+				int[] torso = skin.getRGB(20, 20, 8, 12, null, 0, 8);
+				int[] arm = skin.getRGB(44, 20, 4, 12, null, 0, 4);
+				int[] leg = skin.getRGB(4, 20, 4, 12, null, 0, 4);
+				int[] hat = skin.getRGB(40, 8, 8, 8, null, 0, 8);
+				for (int i = 0; i < hat.length; i++)
+					if (hat[i] == 0)
+						hat[i] = head[i];
+				drawing.setRGB(4, 0, 8, 8, hat, 0, 8);
+				drawing.setRGB(4, 8, 8, 12, torso, 0, 8);
+				drawing.setRGB(0, 8, 4, 12, arm, 0, 4);
+				drawing.setRGB(12, 8, 4, 12, arm, 0, 4);
+				drawing.setRGB(4, 20, 4, 12, leg, 0, 4);
+				drawing.setRGB(8, 20, 4, 12, leg, 0, 4);
+			}
 			ImageIO.write(drawing, "png", skinOut);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
+			if (skinOut.exists()) skinOut.delete();
+			return;
 		}
 	}
 	
@@ -111,31 +109,46 @@ public class SkinTools {
 		r.drawImage(x, y, width, height);
 	}
 	
-	public static void cacheSkins(boolean ingame) {
+	public static void cacheSkins(boolean force) {
 		if (!cachedir.exists() && !cachedir.mkdirs()) {
 			System.err.println("unable to load cachedir");
 			return;
 		}
+		Minecraft mc = Minecraft.getInstance();
 		for (int i = 0; i < AltDatabase.getInstance().getAlts().size(); i++) {
 			AccountData data = AltDatabase.getInstance().getAlts().get(i);
-			if (!ingame) StartupMessageManager.addModMessage("IAS: Updating skin " + data.alias + "...");
+			GLFW.glfwSetWindowTitle(mc.mainWindow.getHandle(), "Minecraft " + SharedConstants.getVersion().getName() + " (IAS: Updating skin " + data.alias + "...)");
 			File file = new File(cachedir, data.alias + ".png");
+			if (force || !file.exists()) {
+				loadFromMojang(mc, data.alias, file);
+			}
+		}
+		GLFW.glfwSetWindowTitle(mc.mainWindow.getHandle(), "Minecraft " + SharedConstants.getVersion().getName());
+	}
+	
+	public static void loadFromMojang(Minecraft mc, String name, File f) {
+		try {
+			InputStream is = new URL("https://api.mojang.com/users/profiles/minecraft/" + name).openStream();
+			InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+			String uuid = GSON.fromJson(isr, JsonObject.class).get("id").getAsString();
+			isr.close();
+			is.close();
+			is = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid).openStream();
+			isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+			String s = GSON.fromJson(isr, JsonObject.class).get("properties").getAsJsonArray().get(0).getAsJsonObject().get("value").getAsString();
+			isr.close();
+			is.close();
+			is = new URL(GSON.fromJson(new String(Base64.getDecoder().decode(s), StandardCharsets.UTF_8), JsonObject.class).get("textures").getAsJsonObject().get("SKIN").getAsJsonObject().get("url").getAsString()).openStream();
+			if (f.exists()) f.delete();
+			Files.copy(is, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			is.close();
+		} catch (Exception ign) {
 			try {
-				InputStream is = new URL("https://api.mojang.com/users/profiles/minecraft/" + data.alias).openStream();
-				InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
-				String uuid = GSON.fromJson(isr, JsonObject.class).get("id").getAsString();
-				isr.close();
+				InputStream is = mc.getResourceManager().getResource(new ResourceLocation("textures/entity/steve.png")).getInputStream();
+				Files.copy(is, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
 				is.close();
-				is = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid).openStream();
-				isr = new InputStreamReader(is, StandardCharsets.UTF_8);
-				String s = GSON.fromJson(isr, JsonObject.class).get("properties").getAsJsonArray().get(0).getAsJsonObject().get("value").getAsString();
-				isr.close();
-				is.close();
-				is = new URL(GSON.fromJson(new String(Base64.getDecoder().decode(s), StandardCharsets.UTF_8), JsonObject.class).get("textures").getAsJsonObject().get("SKIN").getAsJsonObject().get("url").getAsString()).openStream();
-				if (file.exists()) file.delete();
-				Files.copy(is, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-				is.close();
-			} catch (Exception e) {
+			} catch (Throwable e) {
+				e.printStackTrace();
 			}
 		}
 	}
