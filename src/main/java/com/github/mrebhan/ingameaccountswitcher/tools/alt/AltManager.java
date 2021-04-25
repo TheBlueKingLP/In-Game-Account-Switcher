be27f3e76a2153fff484d002a252dd1b17d9c2ae
VidTuGit <60929459+VidTuGit@users.noreply.github.com>
2021-04-25 18:24:15 +0300

    Update to 7.0.6

--- src/main/java/com/github/mrebhan/ingameaccountswitcher/tools/alt/AltManager.java
+++ src/main/java/com/github/mrebhan/ingameaccountswitcher/tools/alt/AltManager.java
@@ -40,7 +40,7 @@ public class AltManager {
 	public Throwable setUser(String username, String password) {
 		Throwable throwable = null;
 		if(!MinecraftClient.getInstance().getSession().getUsername().equals(EncryptionTools.decode(username)) || MinecraftClient.getInstance().getSession().getAccessToken().equals("0")){
-			if (!MinecraftClient.getInstance().getSession().getAccessToken().equals("0"))
+			if (!MinecraftClient.getInstance().getSession().getAccessToken().equals("0") && !ConfigValues.ENABLERELOG)
 			{
 				for (AccountData data : AltDatabase.getInstance().getAlts())
 				{
