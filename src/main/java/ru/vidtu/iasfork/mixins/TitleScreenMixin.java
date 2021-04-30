a290be781bf55cb3f9b772a348830e2ed052de10
VidTuGit <60929459+VidTuGit@users.noreply.github.com>
2021-05-01 01:32:19 +0300

    Update to 7.0.7.

--- src/main/java/ru/vidtu/iasfork/mixins/TitleScreenMixin.java
+++ src/main/java/ru/vidtu/iasfork/mixins/TitleScreenMixin.java
@@ -1,5 +1,8 @@
 package ru.vidtu.iasfork.mixins;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,6 +15,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.Text;
+import the_fireplace.ias.config.ConfigValues;
 import the_fireplace.ias.gui.GuiAccountSelector;
 import the_fireplace.ias.gui.GuiButtonWithImage;
 import the_fireplace.ias.tools.SkinTools;
@@ -19,6 +23,7 @@ import the_fireplace.ias.tools.SkinTools;
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
 	private static boolean skinsLoaded, modMenu = false;
+	private static int textX, textY;
 	protected TitleScreenMixin(Text title) {
 		super(title);
 	}
@@ -26,10 +31,18 @@ public class TitleScreenMixin extends Screen {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
 		if (!skinsLoaded) {
+			modMenu = FabricLoader.getInstance().isModLoaded("modmenu");
 			SkinTools.cacheSkins(false);
 			skinsLoaded = true;
 		}
-		modMenu = FabricLoader.getInstance().isModLoaded("modmenu");
+		try {
+			ScriptEngine engine = new ScriptEngineManager(null).getEngineByName("JavaScript");
+			textX = ((Number) engine.eval(ConfigValues.TEXT_X.replace("%width%", Integer.toString(width)).replace("%height%", Integer.toString(height)))).intValue();
+			textY = ((Number) engine.eval(ConfigValues.TEXT_Y.replace("%width%", Integer.toString(width)).replace("%height%", Integer.toString(height)))).intValue();
+		} catch (Throwable t) {
+			textX = width / 2;
+			textY = height / 4 + 48 + 72 + 12 + (modMenu?32:22);
+		}
 		addButton(new GuiButtonWithImage(width / 2 + 104, height / 4 + 48 + 72 + (modMenu?0:-12), 20, 20, "", btn -> {
 			if (Config.getInstance() == null) {
 				Config.load();
@@ -40,6 +53,6 @@ public class TitleScreenMixin extends Screen {
 
 	@Inject(method = "render", at = @At("TAIL"))
 	public void onRender(int mouseX, int mouseY, float delta, CallbackInfo ci) {
-		drawCenteredString(font, I18n.translate("ias.loggedinas") + " " + minecraft.getSession().getUsername() + ".", width / 2, height / 4 + 48 + 72 + 12 + (modMenu?32:22), 0xFFCC8888);
+		drawCenteredString(font, I18n.translate("ias.loggedinas") + " " + minecraft.getSession().getUsername() + ".", textX, textY, 0xFFCC8888);
 	}
 }
