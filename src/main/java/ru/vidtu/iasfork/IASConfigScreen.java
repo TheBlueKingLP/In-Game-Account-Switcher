cd9c3a0e10eebce95d1368f896d62015ff42e98f
VidTuGit <60929459+VidTuGit@users.noreply.github.com>
2021-05-01 01:38:14 +0300

    Update to 7.0.7.

--- src/main/java/ru/vidtu/iasfork/IASConfigScreen.java
+++ src/main/java/ru/vidtu/iasfork/IASConfigScreen.java
@@ -3,6 +3,7 @@ package ru.vidtu.iasfork;
 import com.mojang.blaze3d.matrix.MatrixStack;
 
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.gui.widget.button.CheckboxButton;
 import net.minecraft.util.text.StringTextComponent;
@@ -13,8 +14,9 @@ import the_fireplace.ias.config.ConfigValues;
 public class IASConfigScreen extends Screen {
 	public final Screen prev;
 	public CheckboxButton caseS, relog;
+	public TextFieldWidget textX, textY;
 	public IASConfigScreen(Screen prev) {
-		super(new StringTextComponent("ias.toml"));
+		super(new StringTextComponent("ias.properties"));
 		this.prev = prev;
 	}
 	
@@ -22,15 +24,28 @@ public class IASConfigScreen extends Screen {
 	protected void init() { 
 		addButton(caseS = new CheckboxButton(width / 2 - font.width(new TranslationTextComponent(ConfigValues.CASESENSITIVE_NAME)) / 2 - 24, 40, 20, 20, new TranslationTextComponent(ConfigValues.CASESENSITIVE_NAME), ConfigValues.CASESENSITIVE));
 		addButton(relog = new CheckboxButton(width / 2 - font.width(new TranslationTextComponent(ConfigValues.ENABLERELOG_NAME)) / 2 - 24, 60, 20, 20, new TranslationTextComponent(ConfigValues.ENABLERELOG_NAME), ConfigValues.ENABLERELOG));
+		addButton(textX = new TextFieldWidget(font, width / 2 - 100, 90, 98, 20, new StringTextComponent("X")));
+		addButton(textY = new TextFieldWidget(font, width / 2 + 2, 90, 98, 20, new StringTextComponent("Y")));
 		addButton(new Button(width / 2 - 75, height - 24, 150, 20, new TranslationTextComponent("gui.done"), btn -> {
 			minecraft.setScreen(prev);
 		}));
+		if (ConfigValues.TEXT_X != null) textX.setValue(ConfigValues.TEXT_X);
+		if (ConfigValues.TEXT_Y != null) textY.setValue(ConfigValues.TEXT_Y);
+	}
+	
+	@Override
+	public void tick() {
+		textX.tick();
+		textY.tick();
+		super.tick();
 	}
 	
 	@Override
 	public void removed() {
-		IAS.CASESENSITIVE_PROPERTY.set(caseS.selected());
-		IAS.ENABLERELOG_PROPERTY.set(relog.selected());
+		ConfigValues.CASESENSITIVE = caseS.selected();
+		ConfigValues.ENABLERELOG = relog.selected();
+		ConfigValues.TEXT_X = textX.getValue();
+		ConfigValues.TEXT_Y = textY.getValue();
 		IAS.syncConfig(true);
 	}
 	
@@ -38,6 +53,7 @@ public class IASConfigScreen extends Screen {
 	public void render(MatrixStack ms, int mx, int my, float delta) {
 		renderBackground(ms);
 		drawCenteredString(ms, font, this.title, width / 2, 10, -1);
+		drawCenteredString(ms, font, new TranslationTextComponent(ConfigValues.TEXT_POS_NAME), width / 2, 80, -1);
 		super.render(ms, mx, my, delta);
 	}
 }
